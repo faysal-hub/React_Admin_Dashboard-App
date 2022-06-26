15b5b9f88abe4cf3498b47ef8f2258d66d860905
faysalwzbm <ovitahsan@gmail.com>
2022-06-26 19:12:19 +0200

    fix the bug: syncfusion version conflict with react version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense(
+  'ORg4AjUWIQA/Gnt2VVhhQlFac1pJWnxLeEx0RWFbb1p6d1RMYltBNQtUQF1hS35VdENiWn9adXRRTmlc'
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
