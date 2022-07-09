cc3dca95dd4c2d48873c1e1a084f2cff47baedd8
faysalwzbm <ovitahsan@gmail.com>
2022-07-09 23:30:47 +0200

    add click event for navbar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -27,7 +28,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } =
+    useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -40,24 +42,48 @@ const Navbar = () => {
       <div className="flex">
         <NavButton
           title="Cart"
-          // customFunc={() => handleClick('cart')}
+          customFunc={() => handleClick('cart')}
           color="blue"
           icon={<FiShoppingCart />}
         />
         <NavButton
           title="Chat"
           dotColor="#03C9D7"
-          // customFunc={() => handleClick('chat')}
+          customFunc={() => handleClick('chat')}
           color="blue"
           icon={<BsChatLeft />}
         />
         <NavButton
           title="Notification"
           dotColor="rgb(254, 201, 15)"
-          // customFunc={() => handleClick('notification')}
+          customFunc={() => handleClick('notification')}
           color="blue"
           icon={<RiNotification3Line />}
         />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Faysal
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   );
