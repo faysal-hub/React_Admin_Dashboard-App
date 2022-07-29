fddd575ee67e94b6c10945cf1cb11f0f91c0b9e6
faysalwzbm <ovitahsan@gmail.com>
2022-07-29 22:59:26 +0200

    add the calander component

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 
-const Calender = () => {
-  return <div>Calender</div>;
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+
+const Scheduler = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+      >
+        <ViewsDirective>
+          {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => (
+            <ViewDirective key={item} option={item} />
+          ))}
+        </ViewsDirective>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
-export default Calender;
+export default Scheduler;
