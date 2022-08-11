28c8bf3ff28dd4e7b7a0e06e8ccf6819e1b34b0a
faysalwzbm <ovitahsan@gmail.com>
2022-08-11 21:58:20 +0200

    add the Earning Bar and Pic Chart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -72,6 +72,7 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
+
       <div className="flex gap-10 flex-wrap justify-center">
         {/* Revenue Updates Card */}
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
@@ -131,6 +132,53 @@ const Ecommerce = () => {
             </div>
             <div>
               <Stacked currentMode={currentMode} width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {/* Earning Bar Chart */}
+          <div
+            className="rounded-2xl md:w-400 p-4 m-3"
+            style={{ backgroundColor: currentColor }}
+          >
+            <div className="flex justify-between items-center ">
+              <p className="font-semibold text-white text-2xl">Earnings</p>
+
+              <div>
+                <p className="text-2xl text-white font-semibold mt-8">
+                  $63,448.78
+                </p>
+                <p className="text-gray-200">Monthly revenue</p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <SparkLine
+                currentColor={currentColor}
+                id="column-sparkLine"
+                height="100px"
+                type="Column"
+                data={SparklineAreaData}
+                width="320"
+                color="rgb(242, 252, 253)"
+              />
+            </div>
+          </div>
+          {/* Earning Pie Chart */}
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
+            <div>
+              <p className="text-2xl font-semibold ">$43,246</p>
+              <p className="text-gray-400">Yearly sales</p>
+            </div>
+
+            <div className="w-40">
+              <Pie
+                id="pie-chart"
+                data={ecomPieChartData}
+                legendVisiblity={false}
+                height="160px"
+              />
             </div>
           </div>
         </div>
